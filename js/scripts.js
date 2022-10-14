cea6f84510852d44e08c69db43ca3280d74bd46e
hanabyu <cherrybyuu@gmail.com>
2022-10-14 16:40:35 -0700

    correct typos

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,33 @@
-const buttonCreate = document.querySelector('.create')
+const buttonCreate = document.querySelector('.create');
 buttonCreate.addEventListener('click', function() {
-    let units = prompt("grid size, less than 100")
+    let units = prompt("Please enter the size of your grid. Max size is 100.");
     if (units > 100) {
-        alert('ERROR! Max grid size is 100')
-        return "ERROR"
+        alert('ERROR! Max grid size is 100');
+        return "ERROR";
     }
-    createGrid(units)
+    createGrid(units);
 })
 
 function createGrid (units) {
-    const container = document.querySelector('.container')
-    const gridContainer = document.createElement('div')
-    gridContainer.classList.add('grid-container')
-    container.appendChild(gridContainer)
-    const buttonDestroy = document.querySelector('.destroy')
+    const container = document.querySelector('.container');
+    const gridContainer = document.createElement('div');
+    gridContainer.classList.add('grid-container');
+    container.appendChild(gridContainer);
+    const buttonDestroy = document.querySelector('.destroy');
     buttonDestroy.addEventListener('click', function() {
-        gridContainer.remove()
+        gridContainer.remove();
     })
     for (i = 0; i < units; i++) {
         const unitContainer = document.createElement('div');
-        unitContainer.classList.add('unit-container')
-        gridContainer.appendChild(unitContainer)
+        unitContainer.classList.add('unit-container');
+        gridContainer.appendChild(unitContainer);
 
         for (j = 0; j < units; j++) {
-            const gridUnits = document.createElement('div')
-            gridUnits.classList.add("grid-units")
-            unitContainer.appendChild(gridUnits)
+            const gridUnits = document.createElement('div');
+            gridUnits.classList.add("grid-units");
+            unitContainer.appendChild(gridUnits);
             gridUnits.addEventListener('mouseover', function() {
-                gridUnits.classList.add('hover')
+                gridUnits.classList.add('hover');
             })  
         }
     } 
